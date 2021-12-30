7a5e0f84b1efb710f864e75ad36b2db607959bf7
Truman Shuck <hello@trumanshuck.com>
2021-12-30 16:21:19 -0500

    Adds favicon maybe

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const Image = require("@11ty/eleventy-img");
+const faviconPlugin = require("eleventy-favicon");
 
 async function imageUrl(src, size) {
   let metadata = await Image(src, {
@@ -12,30 +13,10 @@ async function imageUrl(src, size) {
   return data.url
 }
 
-async function imageShortcode(src, alt, sizes) {
-  let metadata = await Image(src, {
-    outputDir: "_site/img/",
-    widths: [null],
-    formats: ["jpg", "jpeg"]
-  });
-
-  let imageAttributes = {
-    alt,
-    sizes,
-    loading: "lazy",
-    decoding: "async",
-  };
-
-  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-  return Image.generateHTML(metadata, imageAttributes);
-}
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.addNunjucksAsyncShortcode("imageUrl", imageUrl);
   eleventyConfig.addLiquidShortcode("imageUrl", imageUrl);
   eleventyConfig.addJavaScriptFunction("imageUrl", imageUrl);
 
-  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
-  eleventyConfig.addLiquidShortcode("image", imageShortcode);
-  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+  eleventyConfig.addPlugin(faviconPlugin, { destination: "./_site" });
 };
